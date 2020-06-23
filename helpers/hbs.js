022cd665bc13ed8ca69e237619c0c9affdd535e6
Siddhesh <siddhesh.kandalgaonkar@tacto.in>
2020-06-24 01:22:26 +0530

    third commit

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -3,5 +3,29 @@ const moment = require('moment')
 module.exports = {
     formatDate: function (date, format) {
         return moment(date).utc().format(format)
-    }
+    },
+    // editIcon: function (dreamsUser, loggedUser, dreamsId, floating = true) {
+    //     if (dreamsUser._id.toString() == loggedUser._id.toString()) {
+    //         if (floating) {
+    //             return `<a href="/stories/edit/${dreamsId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`
+    //         } else {
+    //             return `<a href="/stories/edit/${dreamsId}"><i class="fas fa-edit"></i></a>`
+    //         }
+    //     } else {
+    //         return ''
+    //     }
+    // },
+
+    select: function (selected, options) {
+        return options
+            .fn(this)
+            .replace(
+                new RegExp(' value="' + selected + '"'),
+                '$& selected="selected"'
+            )
+            .replace(
+                new RegExp('>' + selected + '</option>'),
+                ' selected="selected"$&'
+            )
+    },
 }
--- routes/dreams.js
+++ routes/dreams.js
@@ -24,4 +24,77 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc    Show edit page
+// @route   GET /dreams/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const dream = await Dream.findOne({
+            _id: req.params.id,
+        }).lean()
+
+        if (!dream) {
+            return res.render('error/404')
+        }
+
+        if (dream.user != req.user.id) {
+            res.redirect('/dreamlist')
+        } else {
+            res.render('dreams/edit', {
+                dream,
+            })
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+// @desc    Update dream
+// @route   PUT /dreams/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+        let dream = await Dream.findById(req.params.id).lean()
+
+        if (!dream) {
+            return res.render('error/404')
+        }
+
+        if (dream.user != req.user.id) {
+            res.redirect('/dreamlist')
+        } else {
+            dream = await Dream.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true,
+            })
+
+            res.redirect('/dreamlist')
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+// @desc    Delete dream
+// @route   DELETE /dreams/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        let dream = await Dream.findById(req.params.id).lean()
+
+        if (!dream) {
+            return res.render('error/404')
+        }
+
+        if (dream.user != req.user.id) {
+            res.redirect('/dreamlist')
+        } else {
+            await Dream.remove({ _id: req.params.id })
+            res.redirect('/dreamlist')
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
 module.exports = router
